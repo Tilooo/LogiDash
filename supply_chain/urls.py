1da7dcec832b33abaa2b6cfae4719fd4d5b034d1
Tilooo <moonaviad@gmail.com>
2025-11-13 16:10:08 +0200

    12. Created product list page to display data from database.

--- supply_chain/urls.py
+++ supply_chain/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard_view, name='dashboard'),
+    path('products/', views.product_list_view, name='product-list'),
 ]
--- supply_chain/views.py
+++ supply_chain/views.py
@@ -17,3 +17,16 @@ def dashboard_view(request):
 
     # to render the request, specifying the template and passing the context
     return render(request, 'supply_chain/dashboard.html', context)
+
+
+def product_list_view(request):
+    # to get all product objects from the database
+    products = Product.objects.all()
+
+    # to create the context dictionary to pass the data
+    context = {
+        'products': products
+    }
+
+    # to render the template with the data
+    return render(request, 'supply_chain/product_list.html', context)
